729ed39d0636c396a6de812bde9042b4f37eeee2
Ameliah Utary <ameliah1244@gmail.com>
2024-11-14 03:15:30 +0800

    update

--- miawproject/settings.py
+++ miawproject/settings.py
@@ -121,9 +121,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'miawaccounts.User'
+
 
