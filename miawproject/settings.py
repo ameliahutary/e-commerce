8e499a160084857fb6df36ce36033d39d24e4ce8
Ameliah Utary <ameliah1244@gmail.com>
2024-11-14 03:33:00 +0800

    update

--- miawproject/settings.py
+++ miawproject/settings.py
@@ -77,10 +77,25 @@ WSGI_APPLICATION = 'miawproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Gunakan backend MySQL
+        'NAME': 'miawdatabase',  # Nama database MySQL yang akan digunakan
+        'USER': 'root',  # Username MySQL
+        'PASSWORD': 'asustuf22',  # Password MySQL
+        'HOST': 'localhost',  # Lokasi host MySQL, misalnya 'localhost' jika di komputer lokal
+        'PORT': '3306',  # Port koneksi MySQL, defaultnya adalah 3306
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # Opsi karakter set untuk mendukung emoji dan karakter multibahasa
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  # Opsi untuk mengatur mode SQL
+        },
     }
 }
 
